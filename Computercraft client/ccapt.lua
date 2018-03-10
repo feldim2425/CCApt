63ccac319803e0a3ad28c63b8d0686eafc0d722b
Unknown <feldim2425@gmail.com>
2018-03-10 12:22:17 +0100

    Changed help for "upgrade" and fixed typos

--- Computercraft client/ccapt.lua
+++ Computercraft client/ccapt.lua
@@ -64,12 +64,12 @@ end
 
 
 
-if #args < 1 then
+if #args < 1 or args[1] == "help" then
 	term.clear()
 	local lines = {"Help:", "Usage: "..currentProg .. " ..."}
-	helpLine(lines, "install <programname> [serveradress]","installes a programm from standard servers or from a optional server.")
-	helpLine(lines, "remove <programname>","removes a programm from computer.")
-	helpLine(lines, "upgrade [programname]","upgrades a programm (if installed).")
+	helpLine(lines, "install <programname> [serveradress]","installes a program from standard servers or from a optional server.")
+	helpLine(lines, "remove <programname>","removes a program from computer.")
+	helpLine(lines, "upgrade [programname]","upgrades all installed programes or a specific program (if installed).")
 	helpLine(lines, "add-server <serveradress>","adds a server to your Serverlist.")
 	helpLine(lines, "remove-server <serveradress>","removes a server from your Serverlist.")
 	downScroll(lines)
